3f951676692ec8cc23b4c50d60eccec00501a225
Henning Koch <henning.koch@makandra.de>
2016-11-14 13:41:23 +0100

    Require 'active_record' dependency when loading the library

--- lib/assignable_values.rb
+++ lib/assignable_values.rb
@@ -1,3 +1,4 @@
+require 'active_record'
 require 'assignable_values/errors'
 require 'assignable_values/active_record'
 require 'assignable_values/active_record/restriction/base'
@@ -5,4 +6,3 @@ require 'assignable_values/active_record/restriction/belongs_to_association'
 require 'assignable_values/active_record/restriction/scalar_attribute'
 require 'assignable_values/humanized_value'
 require 'assignable_values/humanizable_string'
-
